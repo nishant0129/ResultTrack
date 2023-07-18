9deed46e82689e87d38121a6cdc57a5ade03d5b7
Nishant Kumar <nishantkumar222555@gmail.com>
2023-07-18 11:41:09 +0530

    Deploying

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
+// const express = require("express");
+// const app = express();
+
+// require("dotenv").config();
+// const dbConfig = require("./config/dbConfig");
+// app.use(express.json());
+// const employeeRoute = require("./routes/employeeRoute");
+// const studentRoute = require("./routes/studentRoute");
+// const resultsRoute = require("./routes/resultsRoute");
+
+// app.use("/api/employee/", employeeRoute);
+// app.use("/api/student/", studentRoute);
+// app.use("/api/results/", resultsRoute);
+// const path = require("path");
+// const port = process.env.PORT || 5000;
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+//   });
+// }
+
+// app.listen(port, () => console.log(`Node JS app listening on port ${port}!`));
+
 const express = require("express");
 const app = express();
 
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
+
 app.use(express.json());
 const employeeRoute = require("./routes/employeeRoute");
 const studentRoute = require("./routes/studentRoute");
@@ -11,17 +37,17 @@ const resultsRoute = require("./routes/resultsRoute");
 app.use("/api/employee/", employeeRoute);
 app.use("/api/student/", studentRoute);
 app.use("/api/results/", resultsRoute);
-const path = require("path");
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => console.log(`Node JS app listening on port ${port}!`));
-const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
+
+
+const path = require("path");
+const port = process.env.PORT||5000
+
+
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get('*' , (req,res) => {
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 app.listen(port, () => console.log(`Node JS app listening on port ${port}!`));
 
